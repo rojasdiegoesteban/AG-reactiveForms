db5e98b1fe1c0b8cb00808f39b2c37af5943783e
Diego Rojas <rojas_diego@outlook.com>
2023-10-04 23:23:48 -0300

    first commit

---
+++ src/app/alumnos-form/alumnos-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnos-form',
+  templateUrl: './alumnos-form.component.html',
+  styleUrls: ['./alumnos-form.component.css']
+})
+export class AlumnosFormComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlumnosFormComponent } from './alumnos-form/alumnos-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
