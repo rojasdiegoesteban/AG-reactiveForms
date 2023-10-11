13cdd957cae33f8eef2b66857c2b9830f016d3c7
Diego Rojas <rojas_diego@outlook.com>
2023-10-11 18:48:18 -0300

    Agrego validaciones en todos los campos

--- src/app/alumnos-form/alumnos-form.component.ts
+++ src/app/alumnos-form/alumnos-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import  { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-alumnos-form',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./alumnos-form.component.css']
 })
 export class AlumnosFormComponent {
+
+  constructor( private fb: FormBuilder ) {}
+
+  formUser = this.fb.group({
+    'nombre': ['', Validators.required],
+    'apellido': ['', Validators.required],
+    'email': ['',[Validators.required, Validators.email]],
+    'codigoPostal': ['', Validators.required]
+  });
+
+  get nombre() {
+    return this.formUser.controls['nombre'];
+  }
+
+  get apellido() {
+    return this.formUser.controls['apellido'];
+  }
+
+  get email() {
+    return this.formUser.controls['email'];
+  }
+
+  get cp() {
+    return this.formUser.controls['codigoPostal'];
+  }
+
+
+  enviarForm(): void {
+    console.log(this.formUser.value);
+    this.formUser.reset();
+  }
+
+  limpiarForm() {
+    this.formUser.reset();
+  }
 
 }
